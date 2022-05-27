a7e9b48ec4047c3d0abc09c2e0b43982775b3397
rvmircea <vmircea1771@yahoo.com>
2022-05-27 11:36:59 +0300

    added UnitTest project

---
+++ Artizanalii.UnitTest/Systems/Controllers/TestBeerController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using FluentAssertions;
+using Artizanalii_Api.Controllers;
+using Artizanalii_Api.Entities.Beer;
+using Artizanalii_Api.Repositories.Beers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Artizanalii.UnitTest.Systems.Controllers;
+
+public class UnitTest1
+{
+    [Fact]
+    public async Task Get_OnSuccess_ReturnsStatusCode200()
+    {
+        //Arrange
+        var mockBeerRepository = new Mock<IBeerRepository>();
+        mockBeerRepository.Setup(service => service.GetAllBeersAsync()).ReturnsAsync(new List<Beer>());
+        var sut = new BeersController(mockBeerRepository.Object);
+        //Act
+        var result = await sut.GetAllBeersAsync();
+        //Assert
+        mockBeerRepository.Verify(service => service.GetAllBeersAsync(), Times.Once);
+    }
+
+    [Fact]
+    public async Task Get_200Success()
+    {   //Arrange
+        var mockBeerRepository = new Mock<IBeerRepository>();
+        mockBeerRepository.Setup(service => service.
+                GetAllBeersAsync())
+            .ReturnsAsync(new List<Beer>() {new Beer() {Name = "TestBeer"}});
+        var sut = new BeersController(mockBeerRepository.Object);
+        //Act
+        var result = await sut.GetAllBeersAsync();
+        
+        //Assert
+        result.Should().BeOfType<ActionResult<ICollection<Beer>>>();
+    }   
+}
